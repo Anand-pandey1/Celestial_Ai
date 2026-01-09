08f0ae93acbf7f00e745fafd0934d8ff999d2a86
Anand Pandey <anand-pandey1@outlook.com>
2026-01-09 10:22:55 +0530

    Day 3. working camera with mediapipe but no and tracking yet and (.venv) environment successfully working

--- core/camera.py
+++ core/camera.py
@@ -1,6 +1,22 @@
 import cv2
+import mediapipe as mp
+import pyautogui
 
+pyautogui.FAILSAFE = True
+
+mp_hands = mp.solutions.hands
+mp_draw = mp.solutions.drawing_utils
+
+hands = mp_hands.Hands(
+    static_image_mode=False,
+    max_num_hands=1,
+    min_detection_confidence=0.7,
+    min_tracking_confidence=0.7
+)
+
+screen_w, screen_h = pyautogui.size()
 camera_running = False
+
 
 def start_camera():
     global camera_running
@@ -12,16 +28,30 @@ def start_camera():
         print("❌ Camera not accessible")
         return
 
-    print("📷 Camera started")
+    print("📷 Camera + Hand Tracking started")
 
     while camera_running:
-        ret, frame = cap.read()
-        if not ret:
+        success, frame = cap.read()
+        if not success:
             break
 
-        cv2.imshow("Celestial AI - Camera", frame)
+        frame = cv2.flip(frame, 1)
+        rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
-        # Press Q to exit camera manually
+        result = hands.process(rgb)
+
+        if result.multi_hand_landmarks:
+            for hand in result.multi_hand_landmarks:
+                mp_draw.draw_landmarks(frame, hand, mp_hands.HAND_CONNECTIONS)
+
+                index_tip = hand.landmark[8]
+                x = int(index_tip.x * screen_w)
+                y = int(index_tip.y * screen_h)
+
+                pyautogui.moveTo(x, y, duration=0.05)
+
+        cv2.imshow("Celestial AI - Hand Tracking", frame)
+
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
--- testcelestial.py
+++ testcelestial.py
@@ -1,14 +1,19 @@
-import pyautogui
-import keyboard
-while True:
-    cmd = input(">> ")
-
-    if cmd == "alt tab":
-        pyautogui.keyDown("alt")
-        pyautogui.press("tab")
-        pyautogui.keyUp("alt")
-
-    elif cmd == "open notepad":
-        pyautogui.hotkey("win", "r")
-        pyautogui.write("notepad")
-        pyautogui.press("enter")
+#import pyautogui
+#import keyboard
+#while True:
+#    cmd = input(">> ")
+#
+#    if cmd == "alt tab":
+#        pyautogui.keyDown("alt")
+#        pyautogui.press("tab")
+#        pyautogui.keyUp("alt")
+#
+#    elif cmd == "open notepad":
+#        pyautogui.hotkey("win", "r")
+#        pyautogui.write("notepad")
+#        pyautogui.press("enter")
+#
+#from mediapipe import tasks
+#print(tasks)
+import mediapipe as mp
+print(mp.solutions.hands)
